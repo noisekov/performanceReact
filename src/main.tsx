364c158607ddf2c6efd101a8adaf04c6c9b88d22
noisekov <noise123q@mail.ru>
2025-03-19 01:55:30 +0300

    feat: add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store.tsx';
 
 const root = document.getElementById('root');
 
@@ -13,9 +11,7 @@ if (!root) throw Error('App fall down');
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
