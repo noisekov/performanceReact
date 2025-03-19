27503b8c03f66a9877b386924ca029fe28586a0f
noisekov <noise123q@mail.ru>
2025-03-19 22:39:07 +0300

    feat: add data

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,5 +1,64 @@
+import { useEffect, useState } from 'react';
+
+interface Idata {
+  name: {
+    official: string;
+  };
+  population: number;
+  region: string;
+  flags: {
+    png: string;
+    alt: string;
+  };
+}
 const Home = () => {
-  return <div>Data not send</div>;
+  const [data, setData] = useState<Idata[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data: Idata[] = await response.json();
+    setData(data);
+    console.log(data);
+  };
+
+  return (
+    <table>
+      <thead></thead>
+      <tbody>
+        <tr className="text-center bg-black">
+          <td className="border-1 border-sky-600 w-1/3 p-2">Name</td>
+          <td className="border-1 border-sky-600 w-1/3 p-2">Population</td>
+          <td className="border-1 border-sky-600 w-1/3 p-2">Region</td>
+          <td className="border-1 border-sky-600 p-2">Flag</td>
+        </tr>
+        {data.map((elem, index) => (
+          <tr key={index}>
+            <td className="text-left border-1 border-sky-600 p-2">
+              {elem.name.official}
+            </td>
+            <td className="text-center border-1 border-sky-600 p-2">
+              {elem.population}
+            </td>
+            <td className="text-center border-1 border-sky-600 p-2">
+              {elem.region}
+            </td>
+            <td className="text-center border-1 border-sky-600 p-2">
+              <img
+                src={elem.flags.png}
+                alt={elem.flags.alt}
+                width="100%"
+                height="100%"
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Home;
